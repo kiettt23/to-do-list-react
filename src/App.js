934402a1625a3aa26ffdf023673a2635e226307a
kiettt23 <kiettt23@gmail.com>
2025-08-17 11:43:02 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import TaskList from "./components/TaskList";
+
+import AddTaskForm from "./components/AddTaskForm";
+import "./App.css";
+
+// Step 1 - Build a static version
+// Step 2 - Identify states
+// - The list of tasks
+// - The text of the new task that user has entered
+// - The value of the checkbox Show incompleted tasks only
+// Step 3 - Handling events
+// - Form submit for a new task
+// - Change a task's status
+// - Removing a task
+// - Toggle show incompleted tasks only
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: "task_1", title: "Learn JS", status: 0 },
+    { id: "task_2", title: "Code a Todo List", status: 0 },
+  ]);
+  const [showIncomplete, setShowIncomplete] = useState(false);
+  const [newTask, setNewTask] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newTask) {
+      const task = {
+        id: Date.now(),
+        title: newTask,
+        status: 0,
+      };
+      setTasks([...tasks, task]);
+      setNewTask("");
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const setTaskStatus = (taskId, status) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === taskId) {
+          return { ...task, status: status ? 1 : 0 };
+        }
+        return task;
+      })
+    );
+  };
+
+  const removeTask = (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title="Todo List" subtitle="Get one item done at a time." />
+
+      <TaskList
+        tasks={tasks}
+        showIncomplete={showIncomplete}
+        setTaskStatus={setTaskStatus}
+        removeTask={removeTask}
+        setShowIncomplete={setShowIncomplete}
+      />
+
+      <AddTaskForm
+        newTask={newTask}
+        handleSubmit={handleSubmit}
+        handleInputChange={handleInputChange}
+      />
     </div>
   );
 }
